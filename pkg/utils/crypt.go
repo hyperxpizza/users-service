4e7de4901ee748c2899b4a8668d9692a4615bdfc
hyperxpizza <wojtek.frackowski@gmail.com>
2022-02-13 19:56:07 +0100

    generate pwd hash

--- pkg/utils/crypt.go
+++ pkg/utils/crypt.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GenerateHashAndSalt(pwd string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
