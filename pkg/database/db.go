160c238d234b7cba79d878624edd749413e0b95e
hyperxpizza <wojtek.frackowski@gmail.com>
2022-02-13 19:12:27 +0100

    database package

---
+++ pkg/database/db.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/hyperxpizza/users-service/pkg/config"
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	*sql.DB
+}
+
+func Connect(cfg *config.Config) (*Database, error) {
+	psqlInfo := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", cfg.Database.User, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.Name)
+
+	database, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	err = database.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{database}, nil
+}
