7aa7c7d86fa71afaf658e9ec16ad56c12a2c26f3
hyperxpizza <wojtek.frackowski@gmail.com>
2022-02-13 19:12:12 +0100

    config package

---
+++ pkg/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Config struct {
+	Database struct {
+		User     string `json:"user"`
+		Password string `json:"password"`
+		Port     int    `json:"port"`
+		Name     string `json:"name"`
+		Host     string `json:"host"`
+	} `json:"database"`
+}
+
+func NewConfig(pathToFile string) (*Config, error) {
+	file, err := os.Open(pathToFile)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var c Config
+
+	err = json.Unmarshal(data, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func (c *Config) PrettyPrint() {
+	data, _ := json.MarshalIndent(c, "", " ")
+	fmt.Println(string(data))
+}
